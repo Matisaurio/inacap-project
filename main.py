c6a41aed8a5c50fc2cac3c1924353ada7469cc79
Matisaurio <maybeaferret@gmail.com>
2022-12-20 18:48:51 -0300

    avance 20-12-2022

--- main.py
+++ main.py
@@ -4,24 +4,27 @@ import tkinter as tk
 from PIL import Image, ImageTk
 from vimba import Vimba
 
+
+with Vimba.get_instance() as vimba:
+    with vimba.get_all_cameras()[0] as cam:
+        camera = cam
+
 def camera_streaming(queue):
     global is_streaming
+    global camera
     is_streaming = True
     print("streaming started")
-    with Vimba.get_instance() as vimba:
-        with vimba.get_all_cameras()[0] as camera:
-            while is_streaming:
-                frame = camera.get_frame()
-                frame = frame.as_opencv_image()
-                im = Image.fromarray(frame)
-                img = ImageTk.PhotoImage(im)
-                queue.put(img) # put the capture image into queue
+    while is_streaming:
+        frame = camera.get_frame(timeout_ms=10)
+        im = Image.fromarray(frame.as_opencv_image())
+        image = ImageTk.PhotoImage(im)
+        lblVideo.config(image=image)
+        lblVideo.image = image
     print("streaming stopped")
 
 def start_streaming():
     start_btn["state"] = "disabled" # disable start button to avoid running the threaded task more than once
     stop_btn["state"] = "normal"    # enable stop button to allow user to stop the threaded task
-    show_streaming()
     threading.Thread(target=camera_streaming, args=(queue,), daemon=True).start()
 
 def stop_streaming():
@@ -34,13 +37,6 @@ def stop_streaming():
     start_btn["state"] = "normal"  # enable start button
 
 # periodical task to show frames in queue
-def show_streaming():
-    global after_id
-    if not queue.empty():
-        image = queue.get()
-        lblVideo.config(image=image)
-        lblVideo.image = image
-    after_id = lblVideo.after(20, show_streaming)
 
 queue = SimpleQueue() # queue for video frames
 after_id = None
